2bd77d686e248bc8343087be87c85fd34a3a5215
Swed0ua <shamagala2001@gmail.com>
2026-01-18 09:34:18 +0200

    feat(temporary main screen with logout)

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useAuth } from '../hooks/authHook';
 import { AuthNavigation } from '../navigation/AuthNavigation';
 
 export function ProtectedRoute() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
   if (!user) return <AuthNavigation />;
-  return <></>;
+  return (
+    <>
+      <TouchableOpacity onPress={() => signOut()} style={styles.signOutButton}>
+        <Text style={{ color: 'white' }}>Sign Out</Text>
+      </TouchableOpacity>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  signOutButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 40,
+    alignItems: 'center',
+  },
+});
